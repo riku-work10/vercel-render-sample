6b3220ca016e77f2770970f564198325f8541957
riku-work10 <ukir19990530@gmail.com>
2025-03-09 22:44:55 +0900

    a

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -11,7 +11,10 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2025_02_06_073900) do
-  create_table "tasks", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "tasks", force: :cascade do |t|
     t.string "name"
     t.boolean "is_done"
     t.datetime "created_at", null: false
